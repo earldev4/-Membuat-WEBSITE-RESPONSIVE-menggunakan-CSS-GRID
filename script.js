1f8382e0d1463df2aaaf054f899114025812e5a1
Deva Ahmad <devaahmad43@gmail.com>
2024-08-13 22:11:28 +0700

    membuat animasi 2

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const list = document.querySelector('nav ul')
 menu_toggle.addEventListener('click',() =>{
     list.classList.toggle('slide');
 });
+
+const card = document.querySelectorAll(".service")
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        entry.target.classList.toggle("serv-animation")
+    })
+})
+
+observer.observe(card[0])
